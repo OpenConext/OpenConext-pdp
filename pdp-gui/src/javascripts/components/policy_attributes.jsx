86f65204263f96341209a03e1006103e1cf626a7
oharsta <oharsta@zilverline.com>
2015-10-08 10:00:47 +0200

    Refactored attributes section to component

---
+++ pdp-gui/src/javascripts/components/policy_attributes.jsx
@@ -0,0 +1,164 @@
+/** @jsx React.DOM */
+
+App.Components.PolicyAttributes = React.createClass({
+
+  markAttributes: function (policy) {
+    policy.attributes = policy.attributes.map(function (attr, index) {
+      attr.index = index;
+      return attr;
+    });
+    return policy;
+  },
+
+  getInitialState: function () {
+    return this.markAttributes(this.props.policy);
+  },
+
+  componentWillReceiveProps: function (nextProps) {
+    this.state = this.markAttributes(nextProps.policy);
+  },
+
+  addAttribute: function (attrName) {
+    var attributes = this.state.attributes || [];
+    var index = attributes.length + 1;
+    attributes.push({name: attrName, value: "", index: index});
+    this.setState({attributes: attributes});
+  },
+
+  removeAttribute: function (name) {
+    var attributes = this.state.attributes || [];
+    attributes = this.state.attributes.filter(function (attribute) {
+      return attribute.name !== name;
+    });
+    this.setState({attributes: attributes});
+  },
+
+  preventProp: function preventProp(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  },
+
+  handleAttributeValueChanged: function (attrName, index) {
+    return function (e) {
+      this.preventProp(e);
+      //change attribute value
+      var attributes = this.state.attributes.map(function (attr) {
+        if (attr.name === attrName && attr.index === index) {
+          attr.value = e.target.value;
+        }
+        return attr;
+      });
+      this.setState({attributes: attributes});
+    }.bind(this);
+  },
+
+  handleRemoveAttributeValue: function (attrName, index) {
+    return function (e) {
+      this.preventProp(e);
+      //remove attribute value
+      var attributes = this.state.attributes.filter(function (attr) {
+        return !(attr.name === attrName && attr.index === index);
+      });
+      this.setState({attributes: attributes});
+    }.bind(this);
+  },
+
+  handleNewAttributeValue: function (attrName) {
+    return function (e) {
+      this.preventProp(e);
+      //change attribute value
+      this.addAttribute(attrName);
+    }.bind(this);
+  },
+
+  handleNewAttribute: function (e) {
+    this.preventProp(e);
+    //change attribute value
+    var attrName = e.target.value;
+    this.addAttribute(attrName);
+  },
+
+  handleRemoveAttribute: function (attrName) {
+    return function (e) {
+      this.preventProp(e);
+      //change attribute value
+      this.removeAttribute(attrName);
+    }.bind(this);
+  },
+
+
+  render: function (attrName, attribute) {
+    return (
+        <div className="value-container" key={"div-" + attrName + "-" + attribute.index}>
+          <input type="text" name="value" className="form-input"
+                 key={attrName + "-" + attribute.index}
+                 value={attribute.value}
+                 placeholder="Attribute value..."
+                 onChange={this.handleAttributeValueChanged(attrName, attribute.index)}/>
+          <a href="#" className="remove" onClick={this.handleRemoveAttributeValue(attrName, attribute.index)}>
+            <i className="fa fa-remove"></i>
+          </a>
+        </div>
+    )
+  },
+
+  renderAttributes: function (policy) {
+    var grouped = _.groupBy(policy.attributes, function (attr) {
+      return attr.name;
+    });
+    var attrNames = Object.keys(grouped);
+    var allowedAttributes = this.props.allowedAttributes.filter(function (attr) {
+      return _.isEmpty(grouped[attr.AttributeId]);
+    });
+    var self = this;
+    var emptyAttributes = policy.attributes.filter(function (attr) {
+      return _.isEmpty(attr.value);
+    });
+    var validClassName = (_.isEmpty(policy.attributes) || emptyAttributes.length > 0) ? "failure" : "success";
+    return (
+        <div className={"form-element "+validClassName}>
+          {
+            attrNames.map(function (attrName, index) {
+              return (
+                  <div key={attrName}>
+                    <p className="label">Attribute</p>
+
+                    <div className="attribute-container">
+                      <input type="text" name="attribute" className="form-input disabled" value={attrName}
+                             disabled="disabled"/>
+                      <a href="#" onClick={self.handleRemoveAttribute(attrName)} className="remove">
+                        <i className="fa fa-remove"></i>
+                      </a>
+                    </div>
+                    <div className="attribute-values">
+                      <p className="label">Values(s)</p>
+                      {
+                        grouped[attrName].map(function (attribute) {
+                          return self.renderAttributeValue(attrName, attribute);
+                        })
+                      }
+                      <a href="#" onClick={self.handleNewAttributeValue(attrName)} className="plus">
+                        <i className="fa fa-plus"></i>
+                        Add a new value...
+                      </a>
+                    </div>
+                  </div>
+              );
+            })
+          }
+          <p className="label">Attribute</p>
+          <select value="" onChange={self.handleNewAttribute}>
+            <option value="" disabled="disabled">Add new attribute....</option>
+            {
+              allowedAttributes.map(function (allowedAttribute) {
+                return (<option value={allowedAttribute.AttributeId}
+                                key={allowedAttribute.AttributeId}>{allowedAttribute.AttributeId}</option>);
+              })
+            }
+          </select>
+        </div>);
+
+  },
+
+
+});
--- pdp-gui/src/javascripts/pages/policy_detail_old.jsx
+++ pdp-gui/src/javascripts/pages/policy_detail_old.jsx
@@ -1,6 +1,14 @@
 /** @jsx React.DOM */
 App.Pages.PolicyDetail = React.createClass({
 
+  markAttributes: function (policy) {
+    policy.attributes = policy.attributes.map(function (attr, index) {
+      attr.index = index;
+      return attr;
+    });
+    return policy;
+  },
+
   componentWillUpdate: function () {
     var node = this.getDOMNode();
     this.shouldScrollBottom = node.scrollTop + node.offsetHeight === node.scrollHeight;
@@ -14,11 +22,11 @@ App.Pages.PolicyDetail = React.createClass({
   },
 
   getInitialState: function () {
-    return this.props.policy;
+    return this.markAttributes(this.props.policy);
   },
 
   componentWillReceiveProps: function (nextProps) {
-    this.state = nextProps.policy;
+    this.state = this.markAttributes(nextProps.policy);
   },
 
   toggleDenyRule: function (e) {
@@ -49,20 +57,21 @@ App.Pages.PolicyDetail = React.createClass({
 
   submitForm: function () {
     var self = this;
-    App.Controllers.Policies.saveOrUpdatePolicy(this.state, function (jqxhr) {
-      jqxhr.isConsumed = true;
-      this.setState({flash: jqxhr.responseJSON.details.name});
-    }.bind(this));
+    if (this.isValidPolicy()) {
+      //remove attributes with empty value
+      App.Controllers.Policies.saveOrUpdatePolicy(this.state, function (jqxhr) {
+        jqxhr.isConsumed = true;
+        this.setState({flash: jqxhr.responseJSON.details.name});
+      }.bind(this));
+    } else {
+      this.setState({allFieldsRequired: true});
+    }
   },
 
   isValidPolicy: function () {
     var policy = this.state;
-    var emptyAttributes = policy.attributes.filter(function (attr) {
-      return _.isEmpty(attr.value);
-    });
-    var validClassName = (_.isEmpty(policy.attributes) || emptyAttributes.length > 0) ? "failure" : "success";
     var inValid = _.isEmpty(policy.name) || _.isEmpty(policy.description) || _.isEmpty(policy.serviceProviderId)
-        || _.isEmpty(policy.attributes) || emptyAttributes.length > 0 || _.isEmpty(policy.denyAdvice);
+        || _.isEmpty(policy.attributes) || _.isEmpty(policy.denyAdvice);
     return !inValid;
   },
 
@@ -147,6 +156,148 @@ App.Pages.PolicyDetail = React.createClass({
           <div className="bottom"></div>
         </div>
     );
+  }
+  ,
+  addAttribute: function (attrName) {
+    var attributes = this.state.attributes || [];
+    var index = attributes.length + 1;
+    attributes.push({name: attrName, value: "", index: index});
+    this.setState({attributes: attributes});
+  },
+
+  removeAttribute: function (name) {
+    var attributes = this.state.attributes || [];
+    attributes = this.state.attributes.filter(function (attribute) {
+      return attribute.name !== name;
+    });
+    this.setState({attributes: attributes});
+  },
+
+  preventProp: function preventProp(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  },
+
+  handleAttributeValueChanged: function (attrName, index) {
+    return function (e) {
+      this.preventProp(e);
+      //change attribute value
+      var attributes = this.state.attributes.map(function (attr) {
+        if (attr.name === attrName && attr.index === index) {
+          attr.value = e.target.value;
+        }
+        return attr;
+      });
+      this.setState({attributes: attributes});
+    }.bind(this);
+  },
+
+  handleRemoveAttributeValue: function (attrName, index) {
+    return function (e) {
+      this.preventProp(e);
+      //remove attribute value
+      var attributes = this.state.attributes.filter(function (attr) {
+        return !(attr.name === attrName && attr.index === index);
+      });
+      this.setState({attributes: attributes});
+    }.bind(this);
+  },
+
+  handleNewAttributeValue: function (attrName) {
+    return function (e) {
+      this.preventProp(e);
+      //change attribute value
+      this.addAttribute(attrName);
+    }.bind(this);
+  },
+
+  handleNewAttribute: function (e) {
+    this.preventProp(e);
+    //change attribute value
+    var attrName = e.target.value;
+    this.addAttribute(attrName);
+  },
+
+  handleRemoveAttribute: function (attrName) {
+    return function (e) {
+      this.preventProp(e);
+      //change attribute value
+      this.removeAttribute(attrName);
+    }.bind(this);
+  },
+
+
+  renderAttributeValue: function (attrName, attribute) {
+    return (
+        <div className="value-container" key={"div-" + attrName + "-" + attribute.index}>
+          <input type="text" name="value" className="form-input"
+                 key={attrName + "-" + attribute.index}
+                 value={attribute.value}
+                 placeholder="Attribute value..."
+                 onChange={this.handleAttributeValueChanged(attrName, attribute.index)}/>
+          <a href="#" className="remove" onClick={this.handleRemoveAttributeValue(attrName, attribute.index)}>
+            <i className="fa fa-remove"></i>
+          </a>
+        </div>
+    )
+  },
+
+  renderAttributes: function (policy) {
+    var grouped = _.groupBy(policy.attributes, function (attr) {
+      return attr.name;
+    });
+    var attrNames = Object.keys(grouped);
+    var allowedAttributes = this.props.allowedAttributes.filter(function (attr) {
+      return _.isEmpty(grouped[attr.AttributeId]);
+    });
+    var self = this;
+    var emptyAttributes = policy.attributes.filter(function (attr) {
+      return _.isEmpty(attr.value);
+    });
+    var validClassName = (_.isEmpty(policy.attributes) || emptyAttributes.length > 0) ? "failure" : "success";
+    return (
+        <div className={"form-element "+validClassName}>
+          {
+            attrNames.map(function (attrName, index) {
+              return (
+                  <div key={attrName}>
+                    <p className="label">Attribute</p>
+
+                    <div className="attribute-container">
+                      <input type="text" name="attribute" className="form-input disabled" value={attrName}
+                             disabled="disabled"/>
+                      <a href="#" onClick={self.handleRemoveAttribute(attrName)} className="remove">
+                        <i className="fa fa-remove"></i>
+                      </a>
+                    </div>
+                    <div className="attribute-values">
+                      <p className="label">Values(s)</p>
+                      {
+                        grouped[attrName].map(function (attribute) {
+                          return self.renderAttributeValue(attrName, attribute);
+                        })
+                      }
+                      <a href="#" onClick={self.handleNewAttributeValue(attrName)} className="plus">
+                        <i className="fa fa-plus"></i>
+                        Add a new value...
+                      </a>
+                    </div>
+                  </div>
+              );
+            })
+          }
+          <p className="label">Attribute</p>
+          <select value="" onChange={self.handleNewAttribute}>
+            <option value="" disabled="disabled">Add new attribute....</option>
+            {
+              allowedAttributes.map(function (allowedAttribute) {
+                return (<option value={allowedAttribute.AttributeId}
+                                key={allowedAttribute.AttributeId}>{allowedAttribute.AttributeId}</option>);
+              })
+            }
+          </select>
+        </div>);
+
   },
 
   renderDenyPermitRule: function (policy) {
@@ -165,14 +316,12 @@ App.Pages.PolicyDetail = React.createClass({
             </div>
             <div className="column-3 middle">
               <p className="info">Permit</p>
-              <em>Permit polices enforce that a only a successful match of the attributes defined will result in a
-                Permit. No match will result in a Deny.
+              <em>Permit polices enforce that a only a successful match of the attributes defined will result in a Permit. No match will result in a Deny.
               </em>
             </div>
             <div className="column-3">
               <p className="info">Deny</p>
-              <em>Deny polices are less common to use. If the attributes in the policy match those of the person trying
-                to login then this will result in a Deny. No match will result in a Permit.
+              <em>Deny polices are less common to use. If the attributes in the policy match those of the person trying to login then this will result in a Deny. No match will result in a Permit.
               </em>
             </div>
           </div>
@@ -199,19 +348,6 @@ App.Pages.PolicyDetail = React.createClass({
     );
   },
 
-
-  renderAttributes:function(policy) {
-    return (<App.Components.PolicyAttributes policy={this.props.policy}/>)
-  },
-  /*
-   <App.Components.Select2Selector
-   defaultValue={policy.serviceProviderId}
-   placeholder={"Select the Service Provider - required"}
-   select2selectorId={"serviceProvider"}
-   options={this.parseEntities(this.props.serviceProviders)}
-   handleChange={this.handleChangeServiceProvider}/>
-   */
-
   renderLogicalRule: function (policy) {
     var allAttributesMustMatch = policy.allAttributesMustMatch;
     return (
@@ -228,16 +364,13 @@ App.Pages.PolicyDetail = React.createClass({
             </div>
             <div className="column-3 middle">
               <p className="info">AND</p>
-              <em>Policies with a logical AND rule enforce that all attributes defined must match those of the person
-                trying to login.</em>
+              <em>Policies with a logical AND rule enforce that all attributes defined must match those of the person trying to login.</em>
             </div>
             <div className="column-3">
               <p className="info">OR</p>
-              <em>Polices defined with a logical OR only require one of the attributes to match the attributes of the
-                person requesting access.</em>
+              <em>Polices defined with a logical OR only require one of the attributes to match the attributes of the person requesting access.</em>
             </div>
-            <em className="note"><sup>*</sup> Note that attribute values with the same attribute name always be
-              evaluated with the logical OR meaning only one matching value is required for this attribute.</em>
+            <em className="note"><sup>*</sup> Note that attribute values with the same attribute name always be evaluated with the logical OR meaning only one matching value is required for this attribute.</em>
           </div>
           <div className="bottom"></div>
         </div>
@@ -252,8 +385,7 @@ App.Pages.PolicyDetail = React.createClass({
   renderFlash: function () {
     if (this.state.flash) {
       return (
-          <div className="flash"><p className="error">{this.state.flash}</p><a href="#" onClick={this.closeFlash}><i
-              className="fa fa-remove"></i></a>
+          <div className="flash"><p className="error">{this.state.flash}</p><a href="#" onClick={this.closeFlash}><i className="fa fa-remove"></i></a>
           </div>
       );
     }
