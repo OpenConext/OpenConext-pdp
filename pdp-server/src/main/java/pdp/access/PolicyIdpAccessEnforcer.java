d0583da24f6f464969172f5e9c8069127b6d94c2
oharsta <oharsta@zilverline.com>
2017-01-11 14:44:59 +0100

    Import removed

--- pdp-server/src/main/java/pdp/access/PolicyIdpAccessEnforcer.java
+++ pdp-server/src/main/java/pdp/access/PolicyIdpAccessEnforcer.java
@@ -1,6 +1,5 @@
 package pdp.access;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
