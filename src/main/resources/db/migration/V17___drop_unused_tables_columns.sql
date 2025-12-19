cd9cfad71aa8f8b210383231a6045c47e4b4a521
Okke Harsta <oharsta@zilverline.com>
2025-12-19 15:07:50 +0100

    Delete index before column

--- src/main/resources/db/migration/V17___drop_unused_tables_columns.sql
+++ src/main/resources/db/migration/V17___drop_unused_tables_columns.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS pdp_migrated_policies;
 DROP TABLE IF EXISTS pdp_policy_violations;
 
 -- Remove obsolete columns from pdp_policies
+ALTER TABLE pdp_policies DROP INDEX pdp_policy_name_revision_unique;
 ALTER TABLE pdp_policies DROP COLUMN authenticating_authority;
 ALTER TABLE pdp_policies DROP COLUMN user_identifier;
 ALTER TABLE pdp_policies DROP COLUMN user_display_name;
