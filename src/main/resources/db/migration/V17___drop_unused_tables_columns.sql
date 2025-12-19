335c9c262b978b8d78321132ffdd172ad1621060
Okke Harsta <oharsta@zilverline.com>
2025-12-19 14:54:41 +0100

    Fix build

--- src/main/resources/db/migration/V17___drop_unused_tables_columns.sql
+++ src/main/resources/db/migration/V17___drop_unused_tables_columns.sql
@@ -3,10 +3,9 @@ DROP TABLE IF EXISTS pdp_migrated_policies;
 DROP TABLE IF EXISTS pdp_policy_violations;
 
 -- Remove obsolete columns from pdp_policies
-ALTER TABLE pdp_policies
-DROP COLUMN authenticating_authority,
-    DROP COLUMN user_identifier,
-    DROP COLUMN user_display_name,
-    DROP COLUMN revision_parent_id,
-    DROP COLUMN revision_nbr,
-    DROP COLUMN latest_revision;
+ALTER TABLE pdp_policies DROP COLUMN authenticating_authority;
+ALTER TABLE pdp_policies DROP COLUMN user_identifier;
+ALTER TABLE pdp_policies DROP COLUMN user_display_name;
+ALTER TABLE pdp_policies DROP COLUMN revision_parent_id;
+ALTER TABLE pdp_policies DROP COLUMN revision_nbr;
+ALTER TABLE pdp_policies DROP COLUMN latest_revision;
